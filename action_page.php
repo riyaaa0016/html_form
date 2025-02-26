b53543c21b859634b48f0af3165ab726bfa2f4e8
rhea <rlpangilinan23-0305@cca.edu.ph>
2025-02-26 19:48:27 +0800

    add this okay
    html_form mo lagay

---
+++ action_page.php
@@ -0,0 +1,37 @@
+<?php
+    echo "<pre>";
+    var_dump($_POST); 
+    echo "</pre>";
+    echo "<br><br>";
+
+    $fullname = $_POST['name'];
+    $gender = $_POST ['gender'];
+    $country = $_POST ['country'];
+?>
+
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database ="wait_lang_sir";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
+
+<?php
+    $sql = "INSERT INTO student_record (id, full_name, gender, country)
+    VALUES (null, '$fullname', '$gender', '$country')";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+?>
--- register.php
+++ register.php
@@ -6,26 +6,28 @@
     <title>Form</title>
 </head>
     <body>
-        <form action="action.php" method="POST">
-            <table border="2">
+        <h1> ADD STUDENT </h1>
+        <form action="action_page.php" method="POST">
+            <table border="1" cellpadding="20" cellspacing="5" width="60%">
                 <tr> 
                     <th> Question </th>
                     <th> Answer </th>
                 <tr>
                     <td> Name: </td>
-                    <td> <input type="text" name="name"> </td>
+                    <td> <input type="text" name="name" placeholder="Enter Full Name"> </td>
                 </tr>
                 <tr>
                     <td> Gender: </td>
                     <td>
-                        <input type="radio" name="gender" value="male"> Male
-                        <input type="radio" name="gender" value="female"> Female
+                        <input type="radio" name="gender" value="male"> Male <br>
+                        <input type="radio" name="gender" value="female"> Female <br>
                     </td>
                 </tr>
                 <tr>
                     <td> Country: </td>
                     <td>
                         <select name="country">
+                            <option value="">SELECT COUNTRY</option>
                             <option value="PH">Philippines</option>
                             <option value="AUS">Australia</option>
                             <option value="US">United States</option>
@@ -33,8 +35,8 @@
                     </td>
                 </tr>
                 <tr>
-                    <td colspan="2">
-                        <input type="submit" value="Save">
+                    <td colspan="2" align="right">
+                        <input style="padding 10px; color: blue" name="submit" type="submit" value="Save">
                     </td>
                 </tr>
             </table>
