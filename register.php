b83c2faef1b1b829fd5e850c8e87e4afc6e99c5c
rhea <rlpangilinan23-0305@cca.edu.ph>
2025-02-26 18:14:50 +0800

    added new file
    added new file to html_form

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+    <body>
+        <form action="action.php" method="POST">
+            <table border="2">
+                <tr> 
+                    <th> Question </th>
+                    <th> Answer </th>
+                <tr>
+                    <td> Name: </td>
+                    <td> <input type="text" name="name"> </td>
+                </tr>
+                <tr>
+                    <td> Gender: </td>
+                    <td>
+                        <input type="radio" name="gender" value="male"> Male
+                        <input type="radio" name="gender" value="female"> Female
+                    </td>
+                </tr>
+                <tr>
+                    <td> Country: </td>
+                    <td>
+                        <select name="country">
+                            <option value="PH">Philippines</option>
+                            <option value="AUS">Australia</option>
+                            <option value="US">United States</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" value="Save">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
